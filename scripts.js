916c0498ba2b4ef7823229ffa8fce7ba5862c389
bernardyyy <130770630+bernardyyy@users.noreply.github.com>
2023-04-14 15:10:18 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const downloadlinks= document.querySelectorAll ("[data-download]");
+
+downloadlinks.forEach (button => 
+{
+const id = button.dataset.download;
+const image= document.getElementById(id);
+const a = document.createElement("a");
+
+a.href = image.src;
+a.download = "";
+a.style.display = "none";
+
+button.addEventListener("click", () =>
+{document.body.appendChild(a);
+a.click();
+document.body.removeChild(a);
+}
+
+)
+
+});

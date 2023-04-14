47f7fab6a82a35764cf980434d1c9d5454c707a5
bernardyyy <130770630+bernardyyy@users.noreply.github.com>
2023-04-14 16:30:32 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,18 @@
-const downloadlinks= document.querySelectorAll ("[data-download]");
+const downloadButtons = document.querySelectorAll("[data-download]");
+const downloadFunction = (a) => {
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
 
-downloadlinks.forEach (button => 
-{
-const id = button.dataset.download;
-const image= document.getElementById(id);
-const a = document.createElement("a");
+downloadButtons.forEach((button) => {
+  const a = document.createElement("a");
+  a.style.display = "none";
 
-a.href = image.src;
-a.download = "";
-a.style.display = "none";
+  a.href = "./img-1.png";
+  a.download = "img-1"
 
-button.addEventListener("click", () =>
-{document.body.appendChild(a);
-a.click();
-document.body.removeChild(a);
-}
+  button.addEventListener("click", () => downloadFunction(a));
 
-)
-
+  button.removeEventListener("click", () => downloadFunction(a));
 });
